741dc351447ba9a6f2aafffa41a83f1efe4982de
nenomernoi505 <nenomernoi505@gmail.com>
2018-11-17 18:53:24 +0300

    задача

--- js.js
+++ js.js
@@ -1,2 +1,17 @@
-let age = prompt("Сколько вам лет?", "");
-alert("Вам - "+age+" лет!!!");
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+    question1 = prompt("Введите обязательную статью в этом месяце", ""),
+    question2 = prompt("Во сколько обойдется?", ""),
+    appData = {
+        money: +money,
+        timeData: time,
+        expenses:{
+            question1: +question2
+                 },
+        optionalExpense: "",
+        income: "",
+        savings: false
+
+    },
+result = (appData.money - appData.expenses.question1) / 30;
+alert("Ваш дневной бюджет составит - " + result + " рублей");
